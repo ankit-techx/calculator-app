c547e86b9758db25179f2134140f4f07243042ab
Ankit Kumar <ankit.k@atriauniversity.edu.in>
2024-07-19 12:28:19 +0530

    added multiplication functionality

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from cal_func import do_addition,do_subtraction
+from multiply import do_multiplication
 
 select_option = input("""
 CALCULTOR
 1. to add
 2. to subtract
+3. to multiply
+                      
 """)
 
 a = int(input('enter 1st no. '))
@@ -13,4 +16,6 @@ if select_option == '1':
     print(f'The result is {do_addition(a,b)}')
 elif select_option == '2':
     print(f'The result is {do_subtraction(a,b)}')
+elif select_option == '3':
+    print(f'The result is {do_multiplication(a,b)}')
 
