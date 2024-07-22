91cf74b97e3028e4c6b00c04acf131d1e807a6d1
Ankit Kumar <ankit.k@atriauniversity.edu.in>
2024-07-22 17:15:12 +0530

    added the area functionality

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from cal_func import do_addition,do_subtraction
 from multiply import do_multiplication
+from area import area_for_rectangle
 
 select_option = input("""
 CALCULTOR
 1. to add
 2. to subtract
 3. to multiply
+4. to area
                       
 """)
 
@@ -18,4 +20,6 @@ elif select_option == '2':
     print(f'The result is {do_subtraction(a,b)}')
 elif select_option == '3':
     print(f'The result is {do_multiplication(a,b)}')
+elif select_option == '4':
+    print(f'The result is {area_for_rectangle(a,b)}')
 
